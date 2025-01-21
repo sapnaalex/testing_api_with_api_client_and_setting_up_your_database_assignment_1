8de376fedddecc870c30aeefd23fd008fccb4f30
unknown <sapna.alex03@gmail.com>
2025-01-21 14:01:13 +0530

    Api testing

--- index.js
+++ index.js
@@ -34,18 +34,37 @@
 
 const express = require('express');
 const { resolve } = require('path');
+const data = require('./data.json'); 
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+app.post('/students/above-threshold', (req, res) => {
+  const { threshold } = req.body; 
+
+  if (typeof threshold !== 'number' || threshold < 0) {
+    return res.status(400).json({ error: "Invalid threshold. Must be a positive number." });
+  }
+  
+  const result = {
+    count: 0,
+    students: [],
+  };
+
+  result.students = data.filter(student => student.total > threshold);
+  result.count = result.students.length;
+
+  res.send(result);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
 
